f28fd65e4e3b3b72f260d87f3092abeb1283b503
Brumagint/Desktop <brumagint@gmail.com>
2020-10-24 14:29:00 -0400

    Pre-Publish details.

--- Source/SZGJX/SZGJXCharacter.h
+++ Source/SZGJX/SZGJXCharacter.h
@@ -93,13 +93,6 @@ protected:
 	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
 	// End of APawn interface
 
-	/* 
-	 * Configures input for touchscreen devices if there is a valid touch interface for doing so 
-	 *
-	 * @param	InputComponent	The input component pointer to bind controls to
-	 * @returns true if touch controls were enabled.
-	 */
-	bool EnableTouchscreenMovement(UInputComponent* InputComponent);
 
 public:
 	/** Returns Mesh1P subobject **/
